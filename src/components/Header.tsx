be4575ee30ad94d89dc45e14a0a148f7a3f7ad90
Gihan-1994 <nggp1994@gmail.com>
2025-03-29 17:02:42 +0530

    Checing for header responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from "react";
+import React   from "react";
 import SearchItem from "@/components/SearchItem";
 import Link from "next/link";
 import CartIcon from "@/components/CartItcon";
@@ -10,7 +10,8 @@ import {ProductContext} from "@/contexts/ProductContext";
 import CartPanel from "@/components/CartPanel";
 
 
-const Header: React.FC = () => {
+
+const Header: React.FC= () => {
 
     const products = useContext(ProductContext);
 
@@ -21,31 +22,31 @@ const Header: React.FC = () => {
     };
 
     return (
-        <header className="bg-gradient-to-b from-gray-600 to-blue-600 text-white shadow">
-            <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-                <Link href="/" className="text-4xl font-bold">
+        <header className="  items-center justify-items-center bg-gradient-to-b from-gray-600 to-blue-600 text-white shadow ">
+            <div className="container mx-auto px-4 py-4  sm:py-4 flex flex-col sm:flex-row justify-between items-center gap-4">
+                <Link href="/" className="md:text-4xl sm:text-2xl font-bold order-1 sm:order-none">
                     E - SHOPPING
                 </Link>
 
-                <div className="relative w-full md:w-1/3 px-4 ">
+                <div className="relative w-full md:w-1/3  sm:w-1/3 px-2 order-3 sm:order-none ">
                     <SearchItem
                         productProps={products}
                         onProductSelected={handleProductSelected}
                     />
                 </div>
 
-                <nav className="flex items-center space-x-8">
+                <nav className="flex flex-row gap-2 items-center space-x-8">
 
-                    <div>
+                    <div className=" overflow-visible sm:relative">
                         <Profile/>
                     </div>
                     <div className="relative">
-                        <h2 className="text-xl font-bold hover:text-blue-500">
+                        <h2 className=" md:text-xl sm:text-xs order-1 sm:order-none font-bold hover:text-blue-500">
                             Your Cart
                         </h2>
                         <div className="relative">
-                            <CartIcon/>
-                            <CartPanel/>
+                            <CartIcon />
+                            <CartPanel />
                         </div>
                     </div>
 
